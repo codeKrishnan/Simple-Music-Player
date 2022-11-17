68025a020bd968338ede696365c570e3f9b7d8e4
CodeKrishnan <sgopikrishnan1@gmail.com>
2022-11-17 22:35:53 +0530

    SimpleMusicApp - Add colors and drawables

--- app/src/main/java/com/codekrishnan/simplemusicplayer/ui/theme/Color.kt
+++ app/src/main/java/com/codekrishnan/simplemusicplayer/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val BlackBackground = Color(0xFF141414)
+val Yellow = Color(0xFFfcff6c)
+val White = Color(0xffffffff)
+val Grey = Color(0xff868686)
